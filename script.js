b23935e52e87738c76b3a469b5b7a8b00a7c71a6
Tunji Mafe <mafeolatunji17@gmail.com>
2023-07-27 15:22:56 +0100

    Refactor css styles

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function openNav() {
-    document.getElementById("nav-bar").style.visibility = "visible";
+    document.getElementById("nav-bar").style.width = "400px";
   };
 function closeNav() {
-    document.getElementById("nav-bar").style.visibility = "hidden";
+    document.getElementById("nav-bar").style.width = '0';
   };
+
+  function scrollUp() {
+  window.scrollTo({
+    top: 0, 
+    left: 0,
+    behavior: "smooth",
+  })
+}
+
