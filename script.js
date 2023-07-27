ff815ba8942a57a3aebcde60360f78489b290217
Tunji Mafe <mafeolatunji17@gmail.com>
2023-07-27 12:08:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementById("nav-bar").style.visibility = "visible";
+  };
+function closeNav() {
+    document.getElementById("nav-bar").style.visibility = "hidden";
+  };
